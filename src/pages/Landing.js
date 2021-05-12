3ca66c467cc8c591bafe2a1715d0c74f78d20294
storrellas <storrellas@gmail.com>
2021-05-12 12:30:28 +0200

    ENH: Adding first requests to API

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -5,6 +5,9 @@ import { InputGroup, FormControl } from 'react-bootstrap';
 
 import './Landing.scss';
 
+// Axios
+import axios from 'axios';
+
 // Font Awesome
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearch, faPlus } from '@fortawesome/free-solid-svg-icons'
@@ -14,9 +17,67 @@ class Landing extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      qParameter: ''
     }
   }
 
+  handleKeyDown(e){    
+    if (e.keyCode === 13)
+      this.performSearch(e)
+  }
+
+  async performSearch(){
+    const { qParameter } = this.state;
+
+    
+    try{
+      const { qParameter } = this.state;
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/api/aw_lots/_search?q=${qParameter}&size=10`)
+      console.log("repsonse ", response.data)
+      /*
+      const options = {
+        mode: 'no-cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      }
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/aw_lots/_search?q=${qParameter}`, options);
+      if (response.ok) { // if HTTP-status is 200-299
+        // get the response body (the method explained below)
+        let json = await response.json();
+        const data = await response.json()
+        console.log("response ", json)
+      } else {
+        alert("HTTP-Error: " + response.status);
+      }
+      /**/
+
+
+
+    }catch(error){
+      console.log("FAILED")
+
+    }  
+    /**/
+    /*
+    const options = {
+      mode: 'no-cors',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    }
+    fetch(`${process.env.REACT_APP_API_URL}/api/aw_lots/_search?q=${qParameter}`, options)
+      .then( response => {
+        console.log("ok ", response)
+        if (!response.ok) { throw response }
+          console.log( "response ", response.json() )  //we only get here if there is no error
+      })
+      .catch( err => {
+        console.log("err", err.text() )
+      })
+    /**/
+
+  }
 
   render() {
     console.log("ReRender")
@@ -35,7 +96,9 @@ class Landing extends React.Component {
               <InputGroup className="mb-3">
                 <FormControl
                   placeholder="Search by artist"                  
-                  style={{ borderRadius: '10px 0 0 10px ' }}/>
+                  style={{ borderRadius: '10px 0 0 10px ' }}
+                  onChange={e => this.setState({ qParameter: e.target.value })}
+                  onKeyDown={e => this.handleKeyDown(e)} />
                 <InputGroup.Append >
                   <InputGroup.Text style={{ background: 'white', borderRadius: ' 0 10px 10px 0'}}>
                     <FontAwesomeIcon icon={faSearch}
